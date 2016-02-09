e00eff410cb861f5a8e289dacfecd539a750227b
Eduardo Almeida <megaedux@me.com>
2016-02-09 04:12:09 +0000

    Added Alamofire.

---
+++ LatteGrab/LatteShareConnection.swift
@@ -0,0 +1,13 @@
+//
+//  LatteShareConnection.swift
+//  LatteGrab
+//
+//  Created by Eduardo Almeida on 09/02/16.
+//  Copyright © 2016 Eduardo Almeida. All rights reserved.
+//
+
+import Cocoa
+
+class LatteShareConnection: NSObject {
+
+}
